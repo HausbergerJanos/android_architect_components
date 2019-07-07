74376dcfc6a0f3878638b58ea438172094d00bfe
Jani <hausberger.janos@codeyard.eu>
2019-07-07 16:06:26 +0200

    Layouts and binding expressions

--- app/src/main/java/com/hausberger/architectcomponents/MainActivity.kt
+++ app/src/main/java/com/hausberger/architectcomponents/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.hausberger.architectcomponents
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.hausberger.architectcomponents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //binding.name.text = "Dan Brown"
+        //binding.email.text = "danbrown@gmail.com"
+        binding.contact = Contact("Dan Brown", "danb@gmail.com")
+        binding.handler = EventHandler(this)
     }
 }
